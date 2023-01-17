0e89f64a6f3f3b9fe1355f09a490e1eca3b0d0f7
Emerson Coutinho <emersoncoutinho@Emersons-MacBook-Pro.local>
2023-01-17 17:11:17 -0300

    ci

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 export * from "./components/Button";
-export * from './utils/themes';
+export * from "./utils/themes";
 import "./styles/global.css";
 
+export function App() {
+  return <h1>aaa</h1>;
+}
